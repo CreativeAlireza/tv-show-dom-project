e7ed940b73ae5913c05ce5d386b747c1fea9c9c6
Alireza Rahimi <ar.dvlpr@outlook.com>
2022-12-27 00:13:52 +0330

    add chooseSeason

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ const tvShowAirDate = document.querySelector('.airdate');
 const tvShowSeasons = document.querySelector('.seasons-number');
 const seasonDropdown = document.querySelector('.season-dropdown');
 const episodeName = document.querySelector('.card-title');
+const season = document.querySelector('.season-dropdown');
 
 tvShowTitle.textContent = NAME_OF_SHOW;
 tvShowAirDate.textContent = AIRDATE;
@@ -26,16 +27,34 @@ episodeName.textContent = firstEl.name;
 
 
 // functions
+
+function chooseSeason(params) {
+    season.innerHTML = "";
+    
+    const episodesNum = episodesNumber(params);
+    Object.keys(episodesNum).map(el => {
+        const li = `
+            <li>
+                <a class="dropdown-item" href="#" value=${el.slice(-1)}>
+                    Season ${el.slice(-1)}
+                </a>
+            </li>
+        `;
+        season.insertAdjacentHTML('beforeend', li);
+    });
+}
+chooseSeason(episodes);
+
+
 function episodesNumber(params){
     const seasons = {};
     let counter = 0;
-    for (let indexSeasons = 0; indexSeasons <= NUMBER_OF_SEASONS; indexSeasons++) {
-        for (let index = 0; index <= params.length; index++) {
+
+    for (let indexSeasons = 0; indexSeasons <= NUMBER_OF_SEASONS; indexSeasons++)
+        for (let index = 0; index <= params.length; index++)
             if(params[index]?.season === indexSeasons)
                 seasons[`season${indexSeasons}`] = ++counter;
-        }
         counter = 0;
-    }
 
     return seasons;
 }
